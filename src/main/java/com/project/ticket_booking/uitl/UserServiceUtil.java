834344caf4a4cb664ec853e60853dee8937a9ab8
aryan2574 <ayush.aryan94@yahoo.com>
2026-05-13 17:35:56 +0200

    Add User service

--- src/main/java/com/project/ticket_booking/uitl/UserServiceUtil.java
+++ src/main/java/com/project/ticket_booking/uitl/UserServiceUtil.java
@@ -1,23 +1,14 @@
 package com.project.ticket_booking.uitl;
 
-import com.project.ticket_booking.entity.Train;
-import com.project.ticket_booking.service.TrainService;
+import org.mindrot.jbcrypt.BCrypt;
 
-import java.util.List;
+public class UserServiceUtil {
 
-public class UserServiceUtil
-{
     public static String hashPassword(String plainPassword) {
         return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
     }
 
     public static boolean checkPassword(String plainPassword, String hashedPassword) {
         return BCrypt.checkpw(plainPassword, hashedPassword);
-    }
-
-    public List<Train> getTrains(String source, String destination) {
-        try {
-            TrainService trainService
-        }
     }
 }
